48e1eaf998128ad130b6383d4d97caba3552d912
batch250251 <ytdred2021@gmail.com>
2021-07-25 12:52:15 +0530

    addition project

---
+++ batch253/org.magnitia.cictaddition/src/test/java/mypack/Test1.java
@@ -0,0 +1,15 @@
+package mypack;
+
+import org.testng.annotations.Test;
+
+public class Test1
+{
+	@Test
+	public void method()
+	{
+		int x=10;
+		int y=20;
+		int z=x+y;
+		System.out.println("Output is "+z);
+	}
+}
